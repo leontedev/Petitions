945221ddf76f924ef585c91ad4358b95fabf3c49
Mihail87 <leontemihai@gmail.com>
2019-09-12 11:34:42 +0300

    Completed Challenge - Day 35

--- Petitions/ViewController.swift
+++ Petitions/ViewController.swift
@@ -11,17 +11,23 @@ import UIKit
 class ViewController: UITableViewController {
     
     var petitions = [Petition]()
-    
+    var filteredPetitions = [Petition]()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Credits", style: .plain, target: self, action: #selector(showCredits))
+        
+        navigationItem.leftBarButtonItem = UIBarButtonItem(title: "Filter", style: .plain, target: self, action: #selector(filterResults))
+        
         var urlString: String
         
         if navigationController?.tabBarItem.tag == 0 {
-            urlString = "https://www.hackingwithswift.com/samples/petitions-1.json"
+            //urlString = "https://www.hackingwithswift.com/samples/petitions-1.json"
+            urlString = "https://api.whitehouse.gov/v1/petitions.json?limit=100"
         } else {
-            urlString = "https://www.hackingwithswift.com/samples/petitions-2.json"
+            //urlString = "https://www.hackingwithswift.com/samples/petitions-2.json"
+            urlString = "https://api.whitehouse.gov/v1/petitions.json?signatureCountFloor=10000&limit=100"
         }
         
         if let url = URL(string: urlString) {
@@ -35,11 +41,33 @@ class ViewController: UITableViewController {
         
     }
     
+    @objc func filterResults() {
+        let ac = UIAlertController(title: "Search", message: "Enter a search string", preferredStyle: .alert)
+        ac.addTextField()
+        ac.addAction(UIAlertAction(title: "Filter", style: .default, handler: { [unowned ac, weak self] _ in
+            guard let searchTerm = ac.textFields?[0].text else { return }
+            self?.title = "\"\(searchTerm)\""
+            self?.filteredPetitions = self?.petitions.filter { $0.title.uppercased().contains(searchTerm.uppercased()) || $0.body.uppercased().contains(searchTerm.uppercased())
+                } ?? []
+            
+            self?.tableView.reloadData()
+        }))
+                     
+        present(ac, animated: true)
+    }
+    
+    @objc func showCredits() {
+        let ac = UIAlertController(title: "Credits", message: "The data comes from We The People API of the Whitehouse", preferredStyle: .alert)
+        ac.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
+        present(ac, animated: true)
+    }
+    
     func parse(data: Data) {
         let decoder = JSONDecoder()
         
         if let petitions = try? decoder.decode(Petitions.self, from: data) {
             self.petitions = petitions.results
+            self.filteredPetitions = petitions.results
             tableView.reloadData()
         }
     }
@@ -53,20 +81,20 @@ class ViewController: UITableViewController {
     }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return petitions.count
+        return filteredPetitions.count
     }
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
-        cell.textLabel?.text = petitions[indexPath.row].title
-        cell.detailTextLabel?.text = petitions[indexPath.row].body
+        cell.textLabel?.text = filteredPetitions[indexPath.row].title
+        cell.detailTextLabel?.text = filteredPetitions[indexPath.row].body
         
         return cell
     }
     
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         let vc = DetailViewController()
-        vc.detailItem = petitions[indexPath.row]
+        vc.detailItem = filteredPetitions[indexPath.row]
         navigationController?.pushViewController(vc, animated: true)
     }
 
